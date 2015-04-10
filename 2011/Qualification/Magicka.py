b65be80a4367cf308bd225be84276728b0a9066e
ap <aprakash@brandeis.edu>
2015-04-10 12:50:44 -0400

    completed the full test of the input.in

--- 2011/Qualification/Magicka.py
+++ 2011/Qualification/Magicka.py
@@ -2,14 +2,14 @@ from __future__ import division
 from itertools import combinations
 from sys import argv
 
-def solve(base_combinations, opposing_bases, sequence):
-
+def solve(arg):
+    base_combinations, opposing_bases, sequence = arg
     sol = []
     for base in sequence:
         sol.append(base)
         # first priority base combination
-        if len(sol) <= 1 : continue
         for i,j in [(-1,-2), (-2,-1)]:
+            if len(sol) <= 1 : continue
             if (sol[i], sol[j]) in base_combinations:
                 new_base = base_combinations[(sol[i], sol[j])]
                 del sol[-1] # remove the last element
@@ -20,11 +20,26 @@ def solve(base_combinations, opposing_bases, sequence):
         sol_set = set(sol)
         for opposing_base_set in opposing_bases:
             if not opposing_base_set - sol_set : sol = []
-        
-
-
     return sol 
+
+def get_data(ln):
+    base_combinations = {}
+    opposing_bases = []
+
+    it =  iter(ln.split(' '))
+    C = int( it.next() )
+    for j in xrange(C):
+        A, B, C= it.next()
+        base_combinations[(A, B)] = C
+
+    D = int( it.next() )
+    for k in xrange(D):
+        opposing_bases.append(set(it.next()))
     
+    N = int( it.next() )
+    sequence = list(it.next())
+    
+    return base_combinations, opposing_bases, sequence
 
 def main():
     filename = 'input.in'
@@ -36,9 +51,10 @@ def main():
         case_count = 0
         for i in xrange(T):
             case_count += 1
-            l = f.readline().rstrip('\n')
-            ans = solve(l)
-            print "Case #" + str(case_count) + ": " + str(ans)
+            ln = f.readline().rstrip('\n')
+            ans = solve(get_data(ln))
+            # print ans
+            print "Case #" + str(case_count) + ": [" + str(', '.join(ans)) + "]"
 
 def demo():
     base_combinations = {}
@@ -51,7 +67,8 @@ def demo():
 
     print solve(base_combinations, opposing_bases, sequence)
 
-# main()
-demo()
+main()
+# get_data('2 EEZ AAB 2 QE ZY 7 QEEEERA')
+# demo()
 
 
